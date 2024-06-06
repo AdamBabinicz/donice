8f7f140b3e22bca366b833a2d043a160f6fbbf06
babinicz <babinicz@onet.pl>
2024-06-06 11:36:20 +0200

    zm

--- src/pages/contact/Form.js
+++ src/pages/contact/Form.js
@@ -14,6 +14,7 @@ const Overlay = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 1000;
+  overflow-y: auto;
 `;
 
 const FormContainer = styled.div`
@@ -21,41 +22,65 @@ const FormContainer = styled.div`
   color: ${({ theme }) => theme.text};
   padding: 20px;
   border-radius: 8px;
-  width: 600px;
+  width: 90%;
+  max-width: 600px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   text-align: center;
   position: relative;
+
+  @media (max-width: 768px) {
+    padding: 15px;
+  }
 `;
 
 const Title = styled.h2`
-  margin-bottom: 20px;
+  margin-bottom: 10px;
 `;
 
 const Subtitle = styled.h3`
-  margin: 20px 0 10px;
+  /* margin: 20px 0 10px; */
+  margin: 5px 0;
 `;
 
 const Input = styled.input`
-  width: calc(50% - 10px);
+  width: 100%;
   padding: 10px;
-  margin: 5px;
+  margin: 5px 0;
   border: 1px solid #ddd;
   border-radius: 4px;
   background: ${({ theme }) => theme.highlight};
   color: ${({ theme }) => theme.text};
 `;
 
+const GroupedInput = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 10px;
+
+  ${Input} {
+    flex: 1 0 30%;
+  }
+
+  @media (max-width: 768px) {
+    ${Input} {
+      flex: 1 0 100%;
+      margin: 5px 0;
+    }
+  }
+`;
+
 const Button = styled.button`
   width: 100%;
   padding: 10px;
-  background-color: ${({ theme }) => theme.text};
+  background: ${({ theme }) => theme.text};
   color: ${({ theme }) => theme.body};
   border: none;
   border-radius: 4px;
   cursor: pointer;
   margin-top: 20px;
   &:hover {
-    background-color: ${({ theme }) => theme.secondaryText};
+    background: ${({ theme }) => theme.secondaryText};
   }
 `;
 
@@ -65,6 +90,10 @@ const CloseButton = styled(FaTimes)`
   right: 10px;
   cursor: pointer;
   color: ${({ theme }) => theme.text};
+`;
+
+const FormSection = styled.div`
+  margin-bottom: 10px;
 `;
 
 const Form = ({ onClose, theme }) => {
@@ -115,102 +144,115 @@ const Form = ({ onClose, theme }) => {
         <CloseButton onClick={onClose} theme={theme} />
         <Title>Formularz zamówienia</Title>
         <form onSubmit={handleSubmit}>
-          <Input
-            type="text"
-            name="name"
-            placeholder="Imię i nazwisko"
-            onChange={handleChange}
-            required
-          />
+          <FormSection>
+            <Input
+              type="text"
+              name="name"
+              placeholder="Imię i nazwisko"
+              onChange={handleChange}
+              required
+            />
+            <Input
+              type="text"
+              name="postalCode"
+              placeholder="Kod pocztowy"
+              onChange={handleChange}
+              required
+            />
+            <Input
+              type="text"
+              name="city"
+              placeholder="Miasto"
+              onChange={handleChange}
+              required
+            />
+          </FormSection>
 
-          <Subtitle>Dane adresowe</Subtitle>
-          <Input
-            type="text"
-            name="postalCode"
-            placeholder="Kod pocztowy"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="text"
-            name="city"
-            placeholder="Miasto"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="text"
-            name="street"
-            placeholder="Nazwa ulicy"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="text"
-            name="houseNumber"
-            placeholder="Nr domu"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="text"
-            name="apartmentNumber"
-            placeholder="Nr mieszkania"
-            onChange={handleChange}
-          />
+          <FormSection>
+            <GroupedInput>
+              <Input
+                type="text"
+                name="street"
+                placeholder="Nazwa ulicy"
+                onChange={handleChange}
+                required
+              />
+              <Input
+                type="text"
+                name="houseNumber"
+                placeholder="Nr domu"
+                onChange={handleChange}
+                required
+              />
+              <Input
+                type="text"
+                name="apartmentNumber"
+                placeholder="Nr mieszkania"
+                onChange={handleChange}
+              />
+            </GroupedInput>
+          </FormSection>
 
-          <Subtitle>Kontakt</Subtitle>
-          <Input
-            type="tel"
-            name="phone"
-            placeholder="Telefon"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={handleChange}
-            required
-          />
+          <FormSection>
+            <Subtitle>Kontakt</Subtitle>
+            <Input
+              type="tel"
+              name="phone"
+              placeholder="Telefon"
+              onChange={handleChange}
+              required
+            />
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              required
+            />
+          </FormSection>
 
-          <Subtitle>Wymiary i kolor donicy</Subtitle>
-          <Input
-            type="text"
-            name="length"
-            placeholder="Długość w cm"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="text"
-            name="width"
-            placeholder="Szerokość w cm"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="text"
-            name="height"
-            placeholder="Wysokość w cm"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="text"
-            name="color"
-            placeholder="Kolor"
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="number"
-            name="quantity"
-            placeholder="Ilość"
-            onChange={handleChange}
-            required
-          />
+          <FormSection>
+            <Subtitle>Wymiary i kolor donicy</Subtitle>
+            <GroupedInput>
+              <Input
+                type="text"
+                name="length"
+                placeholder="Długość w cm"
+                onChange={handleChange}
+                required
+              />
+              <Input
+                type="text"
+                name="width"
+                placeholder="Szerokość w cm"
+                onChange={handleChange}
+                required
+              />
+            </GroupedInput>
+            <GroupedInput>
+              <Input
+                type="text"
+                name="height"
+                placeholder="Wysokość w cm"
+                onChange={handleChange}
+                required
+              />
+              <Input
+                type="text"
+                name="color"
+                placeholder="Kolor"
+                onChange={handleChange}
+                required
+              />
+              <Input
+                type="number"
+                name="quantity"
+                placeholder="Ilość"
+                onChange={handleChange}
+                required
+              />
+            </GroupedInput>
+          </FormSection>
 
           <Button type="submit" theme={theme}>
             Wyślij
